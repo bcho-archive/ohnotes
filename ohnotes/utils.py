aa2b900e3f420624929ac07227eb456410e6a541
hbc <bcxxxxxx@gmail.com>
2012-11-27 23:33:07 +0800

    fixed unicode

--- ohnotes/utils.py
+++ ohnotes/utils.py
@@ -7,7 +7,7 @@ def read_list(fname, parent_path):
     '''read a list from a file'''
     path = os.path.join(parent_path, fname)
     try:
-        return [i.strip() for i in open(path, 'r').readlines()]
+        return [unicode(i.strip()) for i in open(path, 'r').readlines()]
     #: always return a list
     except:
         return []
